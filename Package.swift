01cf747e615d93e24f7c0a63ad5ca1a8538ecf1a
Damien Morard <damien.morard@unige.ch>
2019-09-23 16:42:46 +0200

    Begin update to swift 5.0

--- Package.swift
+++ Package.swift
@@ -1,3 +1,14 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
-let package = Package(name: "SwiftKanren")
+let package = Package(
+  name: "SwiftKanren",
+  products: [
+    .library(name: "SwiftKanren", type: .static, targets: ["SwiftKanren"]),
+  ],
+  targets: [
+    .target(name: "SwiftKanren"),
+    .testTarget(name: "SwiftKanrenTests", dependencies: ["SwiftKanren"]),
+  ]
+)
