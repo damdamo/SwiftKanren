f333e4b26bc26cf36fb034ed5627ad35001d8b37
Damien Morard <damien.morard@unige.ch>
2019-09-24 14:35:56 +0200

    Rename SwiftKanren -> LogicKit

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftKanren",
+  name: "LogicKit",
   products: [
     .library(name: "LogicKit", type: .static, targets: ["SwiftKanren"]),
   ],
